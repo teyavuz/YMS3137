6230f36e61f4d8873cc9619aea96b85a5d563741
fatih günalp <fatih.gunalp@outlook.com>
2020-02-19 10:38:21 +0300

    görevler tanımlandı.

--- WindowsFormsApp1/WindowsFormsApp1/deneme.cs
+++ WindowsFormsApp1/WindowsFormsApp1/deneme.cs
@@ -9,5 +9,25 @@ namespace WindowsFormsApp1
     class deneme
     {
         //cem tarafından olusturuldu.
+
+        //Mert buraya string değer döndüren bir metot yaz.
+
+        //ahmet burada int değer döndüren bir metot yaz.
+
+        //ihsan bir metot yaz içinde mesaj kutusunda merhaba yazsın.
+
+        //bilal datetime tipinde bir property oluştur.
+
+        //esat  decimal Fiyat isminde bir metot oluştur.
+
+        //hava 3 farklı property oluştur
+
+        //gülsen temizleme metodu yaz.
+
+        //emirhan bir adet property oluştur.
+
+        //reyhan herhangi bir metot oluştur.
+
+        //şeyma enum oluştur içerisinde admin, kullanıcı ve ziyaretçi olsun.
     }
 }
