0a400f7930605999c5bcfb6a79e007f5e34ee293
fatih günalp <fatih.gunalp@outlook.com>
2020-02-20 12:49:46 +0300

    Sorgular

---
+++ EntityFramework/20.02/WFA_EFSorgular/WFA_EFSorgular/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace WFA_EFSorgular
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOrnek1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnOrnek2 = new System.Windows.Forms.Button();
+            this.btnOrnek3 = new System.Windows.Forms.Button();
+            this.btnOrnek4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOrnek1
+            // 
+            this.btnOrnek1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnOrnek1.Location = new System.Drawing.Point(12, 12);
+            this.btnOrnek1.Name = "btnOrnek1";
+            this.btnOrnek1.Size = new System.Drawing.Size(139, 36);
+            this.btnOrnek1.TabIndex = 0;
+            this.btnOrnek1.Text = "Sorgu 1";
+            this.btnOrnek1.UseVisualStyleBackColor = true;
+            this.btnOrnek1.Click += new System.EventHandler(this.btnOrnek1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 54);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1104, 559);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnOrnek2
+            // 
+            this.btnOrnek2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnOrnek2.Location = new System.Drawing.Point(157, 12);
+            this.btnOrnek2.Name = "btnOrnek2";
+            this.btnOrnek2.Size = new System.Drawing.Size(139, 36);
+            this.btnOrnek2.TabIndex = 0;
+            this.btnOrnek2.Text = "Sorgu 2";
+            this.btnOrnek2.UseVisualStyleBackColor = true;
+            this.btnOrnek2.Click += new System.EventHandler(this.btnOrnek2_Click);
+            // 
+            // btnOrnek3
+            // 
+            this.btnOrnek3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnOrnek3.Location = new System.Drawing.Point(302, 12);
+            this.btnOrnek3.Name = "btnOrnek3";
+            this.btnOrnek3.Size = new System.Drawing.Size(139, 36);
+            this.btnOrnek3.TabIndex = 0;
+            this.btnOrnek3.Text = "Sorgu 3";
+            this.btnOrnek3.UseVisualStyleBackColor = true;
+            this.btnOrnek3.Click += new System.EventHandler(this.btnOrnek3_Click);
+            // 
+            // btnOrnek4
+            // 
+            this.btnOrnek4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnOrnek4.Location = new System.Drawing.Point(447, 12);
+            this.btnOrnek4.Name = "btnOrnek4";
+            this.btnOrnek4.Size = new System.Drawing.Size(139, 36);
+            this.btnOrnek4.TabIndex = 0;
+            this.btnOrnek4.Text = "Sorgu 4";
+            this.btnOrnek4.UseVisualStyleBackColor = true;
+            this.btnOrnek4.Click += new System.EventHandler(this.btnOrnek4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1128, 637);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnOrnek4);
+            this.Controls.Add(this.btnOrnek3);
+            this.Controls.Add(this.btnOrnek2);
+            this.Controls.Add(this.btnOrnek1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOrnek1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnOrnek2;
+        private System.Windows.Forms.Button btnOrnek3;
+        private System.Windows.Forms.Button btnOrnek4;
+    }
+}
+
---
+++ EntityFramework/20.02/WFA_EFSorgular/WFA_EFSorgular/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA_EFSorgular
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        NorthwindEntities db = new NorthwindEntities();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Entity Framework
+
+            /*
+                3 yaklaşım
+                    --DBFirst
+                        --ORM (Object Relational Mapping)
+                            --Veritabanında bulunan tabloları class haline ve tablo içerisindeki kolonları da property haline dönüştüren teknoloji.
+                    --CodeFirst
+                        -- tamamen kod taraflı veritabanı oluşturma ve veritabanına ulaşma..
+                    --ModelFirst
+
+             */
+        }
+
+        private void btnOrnek1_Click(object sender, EventArgs e)
+        {
+            //--Fiyatı 20 ile 50 arasında olan ürünlerin id, ürün adı, fiyatı, stok miktarı, kategori adını getiren sorguyu yazınız.
+
+            //T-Sql Sorgusu
+            /*
+             select p.ProductID,p.ProductName,p.UnitPrice,p.UnitsInStock,c.CategoryName from Products p
+inner join Categories c
+on p.CategoryID=c.CategoryID
+where p.UnitPrice between 20 and 50
+             */
+
+
+            //Linq To Sql
+
+            //var result = from p in db.Products
+            //             where p.UnitPrice > 20 && p.UnitPrice < 50
+            //             orderby p.UnitPrice descending
+            //             select new
+            //             {
+            //                ÜrünID= p.ProductID,
+            //                ÜrünAdı= p.ProductName,
+            //                ÜrünFiyat= p.UnitPrice,
+            //                ÜrünStok= p.UnitsInStock,
+            //                KategoriAdı=p.Category.CategoryName
+            //             };
+            //dataGridView1.DataSource = result.ToList();
+
+
+            //Linq To Entity
+
+            //Lambda Calculus
+
+            var result = db.Products.Where(p => p.UnitPrice > 20 && p.UnitPrice < 50).Select(p => new
+            {
+                ÜrünID = p.ProductID,
+                ÜrünAdı = p.ProductName,
+                ÜrünFiyat = p.UnitPrice,
+                ÜrünStok = p.UnitsInStock,
+                KategoriAdı = p.Category.CategoryName
+            }).ToList();
+
+            dataGridView1.DataSource = result;
+
+
+
+
+
+        }
+
+        private void btnOrnek2_Click(object sender, EventArgs e)
+        {
+            //Siparişler tablosundan Müşteri şirket adı, çalışan adı ve soyadı, sipariş ıd, sipariş tarihi ve kargo şirket adını getiren sorgu.
+
+            //T-Sql
+            /*
+             select c.CompanyName,e.FirstName, e.LastName,o.OrderID,o.ShippedDate,s.CompanyName from Orders o
+                join Employees e on o.EmployeeID=e.EmployeeID
+                join Customers c on c.CustomerID=o.CustomerID
+            join Shippers s on s.ShipperID=o.ShipVia
+             */
+
+            //Linq To Sql
+            //var result = from o in db.Orders
+            //             select new
+            //             {
+            //                 o.Customer.CompanyName,
+            //                 ÇalışanAdıSoyadı = o.Employee.FirstName + " " + o.Employee.LastName,
+            //                 o.OrderID,
+            //                 o.ShippedDate,
+            //                KargoŞirketi= o.Shipper.CompanyName,
+            //             };
+            //dataGridView1.DataSource = result.ToList();
+
+            //Linq To Entity
+            var result = db.Orders.Select(o => new
+            {
+                o.Customer.CompanyName,
+                ÇalışanAdıSoyadı = o.Employee.FirstName + " " + o.Employee.LastName,
+                o.OrderID,
+                o.ShippedDate,
+                KargoŞirketi = o.Shipper.CompanyName
+            });
+            dataGridView1.DataSource = result.ToList();
+        }
+
+        private void btnOrnek3_Click(object sender, EventArgs e)
+        {
+            //Müşteriler tablosu içerisinde CompanyName kolonunda "restaurant" geçen müşterileri listeleyin.
+
+            //T-Sql
+            //select * from Customers where CompanyName like '%restaurant%'
+
+            //Linq To Sql
+            //var result = from c in db.Customers where c.CompanyName.Contains("restaurant") select c;
+            //dataGridView1.DataSource = result.ToList();
+
+            //Linq To Entity
+            dataGridView1.DataSource = db.Customers.Where(x => x.CompanyName.Contains("restaurant")).ToList();
+
+
+            //Not: Detaylı anlatım için https://www.entityframeworktutorial.net/
+        }
+
+        private void btnOrnek4_Click(object sender, EventArgs e)
+        {
+            //Kategorisi Beverages olan ve ürün adı "Armut" fiyatı 10, stok adeti 500 olan ürünü ekleyin.
+
+            //Product product = new Product();
+            //product.ProductName = "Armut";
+            //product.UnitPrice = 10;
+            //product.UnitsInStock = 500;
+
+            //int catId = db.Categories.FirstOrDefault(x => x.CategoryName == "beverages").CategoryID;
+
+            //product.CategoryID = catId;
+            //db.Products.Add(product);
+            //db.SaveChanges();
+            //MessageBox.Show("Ürün Eklendi");
+
+            //Object Initializer
+
+            db.Products.Add(
+                new Product
+                {
+                    ProductName = "Elma",
+                    UnitPrice = 5,
+                    UnitsInStock = 1000,
+                    CategoryID = db.Categories.FirstOrDefault(x => x.CategoryName == "beverages").CategoryID
+                }
+                );
+            db.SaveChanges();
+
+
+
+        }
+    }
+}
