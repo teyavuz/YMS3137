6d06f3726f79b9b65aa9f5349dcf16a93d6db4d9
fatih günalp <fatih.gunalp@outlook.com>
2020-02-26 12:12:27 +0300

    BLL oluşturuldu.

--- EntityFramework/26.02/NTierNorthwindProject/NTierNorthwindProject.BLL/Repositories/Service/CategoryService.cs
+++ EntityFramework/26.02/NTierNorthwindProject/NTierNorthwindProject.BLL/Repositories/Service/CategoryService.cs
@@ -3,10 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NTierNorthwindProject.DAL.MODEL;
 
 namespace NTierNorthwindProject.BLL.Repositories.Service
 {
     public class CategoryService
     {
+        NorthwindEntities db = new NorthwindEntities();
+
+        //Ekleme
+
+        public void Add(Category category)
+        {
+            if (category != null)
+            {
+                db.Categories.Add(category);
+                db.SaveChanges();
+            }
+
+        }
+
+        //Delete
+        public void Delete(int categoryId)//11
+        {
+            //var silinecek= db.Categories.Find(categoryId);
+            var silinecek = SelectById(categoryId);
+            if (silinecek != null)
+            {
+                db.Categories.Remove(silinecek);
+                db.SaveChanges();
+            }
+        }
+
+        //Update
+        public void Update(Category category)
+        {
+            //Todo: İşlemi kontrol et.
+            //var guncellenecek = db.Categories.Find(category.CategoryID);//10
+
+            //guncellenecek.CategoryName = category.CategoryName;
+            //guncellenecek.Description = category.Description;
+
+
+
+            db.Entry(category).State = System.Data.Entity.EntityState.Modified;
+
+            db.SaveChanges();
+
+        }
+
+        //Listeleme
+
+        public List<Category> SelectAll()
+        {
+            return db.Categories.ToList();
+        }
+
+        //Tekil veri getirme
+
+        public Category SelectById(int id)
+        {
+            return db.Categories.Find(id);
+        }
+
+
     }
 }
--- EntityFramework/26.02/NTierNorthwindProject/NTierNorthwindProject.WFA/Form1.cs
+++ EntityFramework/26.02/NTierNorthwindProject/NTierNorthwindProject.WFA/Form1.cs
@@ -1,4 +1,5 @@
-﻿using NTierNorthwindProject.DAL.MODEL;
+﻿using NTierNorthwindProject.BLL.Repositories.Service;
+using NTierNorthwindProject.DAL.MODEL;
 using System;
 using System.Linq;
 using System.Windows.Forms;
@@ -15,8 +16,27 @@ namespace NTierNorthwindProject.WFA
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            #region Örnek Veri Ekleme
             //NorthwindEntities db = new NorthwindEntities();
-            //dataGridView1.DataSource = db.Categories.ToList();
+            //Category category = new Category();
+            //category.CategoryName = "Test 1";
+            //category.Description = "Description 1";
+
+            //db.Categories.Add(category);
+            //db.SaveChanges();
+
+
+            //dataGridView1.DataSource = db.Categories.ToList(); 
+            #endregion
+
+            //CategoryService categoryService = new CategoryService();
+            //Category category = new Category();
+            //category.CategoryName = "Test 1";
+            //category.Description = "Description 1";
+
+            //categoryService.Add(category);
+
+
         }
     }
 }
