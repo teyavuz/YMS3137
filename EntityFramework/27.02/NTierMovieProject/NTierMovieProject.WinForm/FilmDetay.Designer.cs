a29b48d20b5e3b5b5e1aee5bab2f6bbb92c6d4c2
fatih günalp <fatih.gunalp@outlook.com>
2020-03-02 10:56:45 +0300

    Film Detay formu oluşturuldu

---
+++ EntityFramework/27.02/NTierMovieProject/NTierMovieProject.WinForm/FilmDetay.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace NTierMovieProject.WinForm
+{
+    partial class FilmDetay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblYonetmen = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblDil = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblCikisTarihi = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblSure = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblKategori = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblOyuncular = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblOzet = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.lblOyuncular);
+            this.groupBox1.Controls.Add(this.lblKategori);
+            this.groupBox1.Controls.Add(this.lblSure);
+            this.groupBox1.Controls.Add(this.lblCikisTarihi);
+            this.groupBox1.Controls.Add(this.lblDil);
+            this.groupBox1.Controls.Add(this.lblYonetmen);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(693, 560);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Film Detay";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(178, 209);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(217, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Yönetmen:";
+            // 
+            // lblYonetmen
+            // 
+            this.lblYonetmen.AutoSize = true;
+            this.lblYonetmen.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblYonetmen.Location = new System.Drawing.Point(299, 28);
+            this.lblYonetmen.Name = "lblYonetmen";
+            this.lblYonetmen.Size = new System.Drawing.Size(76, 17);
+            this.lblYonetmen.TabIndex = 1;
+            this.lblYonetmen.Text = "Yönetmen:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(217, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Dil:";
+            // 
+            // lblDil
+            // 
+            this.lblDil.AutoSize = true;
+            this.lblDil.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDil.Location = new System.Drawing.Point(299, 58);
+            this.lblDil.Name = "lblDil";
+            this.lblDil.Size = new System.Drawing.Size(24, 17);
+            this.lblDil.TabIndex = 1;
+            this.lblDil.Text = "Dil";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(217, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 17);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Çıkış Tarihi:";
+            // 
+            // lblCikisTarihi
+            // 
+            this.lblCikisTarihi.AutoSize = true;
+            this.lblCikisTarihi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCikisTarihi.Location = new System.Drawing.Point(299, 94);
+            this.lblCikisTarihi.Name = "lblCikisTarihi";
+            this.lblCikisTarihi.Size = new System.Drawing.Size(41, 17);
+            this.lblCikisTarihi.TabIndex = 1;
+            this.lblCikisTarihi.Text = "Tarih";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(217, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 17);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Süre:";
+            // 
+            // lblSure
+            // 
+            this.lblSure.AutoSize = true;
+            this.lblSure.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSure.Location = new System.Drawing.Point(299, 127);
+            this.lblSure.Name = "lblSure";
+            this.lblSure.Size = new System.Drawing.Size(36, 17);
+            this.lblSure.TabIndex = 1;
+            this.lblSure.Text = "süre";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(217, 165);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 17);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Kategori:";
+            // 
+            // lblKategori
+            // 
+            this.lblKategori.AutoSize = true;
+            this.lblKategori.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKategori.Location = new System.Drawing.Point(299, 165);
+            this.lblKategori.Name = "lblKategori";
+            this.lblKategori.Size = new System.Drawing.Size(59, 17);
+            this.lblKategori.TabIndex = 1;
+            this.lblKategori.Text = "kategori";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(216, 202);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 17);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Oyuncular:";
+            // 
+            // lblOyuncular
+            // 
+            this.lblOyuncular.AutoSize = true;
+            this.lblOyuncular.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblOyuncular.Location = new System.Drawing.Point(298, 202);
+            this.lblOyuncular.Name = "lblOyuncular";
+            this.lblOyuncular.Size = new System.Drawing.Size(70, 17);
+            this.lblOyuncular.TabIndex = 1;
+            this.lblOyuncular.Text = "oyuncular";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblOzet);
+            this.groupBox2.Location = new System.Drawing.Point(12, 243);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(675, 311);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Özet";
+            // 
+            // lblOzet
+            // 
+            this.lblOzet.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblOzet.Location = new System.Drawing.Point(6, 36);
+            this.lblOzet.Name = "lblOzet";
+            this.lblOzet.Size = new System.Drawing.Size(663, 272);
+            this.lblOzet.TabIndex = 1;
+            this.lblOzet.Text = "Özet yazı";
+            // 
+            // FilmDetay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 565);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FilmDetay";
+            this.Text = "FilmDetay";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblDil;
+        private System.Windows.Forms.Label lblYonetmen;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblCikisTarihi;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblSure;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblOzet;
+        private System.Windows.Forms.Label lblOyuncular;
+        private System.Windows.Forms.Label lblKategori;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+    }
+}
