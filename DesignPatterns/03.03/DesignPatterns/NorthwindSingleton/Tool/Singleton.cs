f52acd91212faeea4f0bc6510dd08f6c3d3304c2
fatih günalp <fatih.gunalp@outlook.com>
2020-03-03 12:55:51 +0300

    northwind singleton

---
+++ DesignPatterns/03.03/DesignPatterns/NorthwindSingleton/Tool/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using NorthwindSingleton.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindSingleton.Tool
+{
+    public class Singleton
+    {
+        private Singleton() { }
+
+        private static NorthwindEntities _db;
+
+        public static NorthwindEntities db
+        {
+            get
+            {
+                if (_db == null)
+                    _db = new NorthwindEntities();
+                return _db;
+            }
+        }
+
+
+    }
+}
