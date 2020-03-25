ce67690dda280e18039b203e785a956f9a5c0074
fatih günalp <fatih.gunalp@outlook.com>
2020-03-25 12:19:51 +0300

    .

---
+++ GenelTekrar/25.03/CodeFirstSample2/CodeFirstSample2/Context/CodeFirstContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;//bu kütüphane DbContext nesnesini barındıran kütüphanedir.
+using CodeFirstSample2.Entity;
+
+namespace CodeFirstSample2.Context
+{
+    public class CodeFirstContext:DbContext
+    {
+        //Constructor => Yapıcı Metot.
+
+        /*
+         Nuget Package manager ile entityframework projeye dahil edilir.
+         Entity ve Context isminde klasörler oluşturulur.
+         Entity klasörü içerisinde tablo haline gelecek olan classlar tanımlanır.
+         Context klasörüsü içerisine ContextClassı oluşturulur.
+         Context classı System.Data.Entity kütüphanesi ile birlikte DBContext classından miras alır.
+         tablo haline gelmesini istediğimiz classlar DbSet<Product> şeklinde propertyler tanımlanırç.
+         Nuget Console açılarak "enable-migrations" komutu yazılır
+         Son olarak yine console ekranın "update-database" yazılarak veritabanı oluşturulur.
+         */
+        public CodeFirstContext()
+        {
+            Database.Connection.ConnectionString = "server=.;database=codefirstsample2;uid=sa;pwd=123";
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        //Fluent Api.
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ GenelTekrar/25.03/CodeFirstSample2/CodeFirstSample2/Entity/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeFirstSample2.Entity
+{
+    public class Category
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        [MaxLength(50)]
+        public string CategoryName { get; set; }
+        [MaxLength(250)]
+        public string Description { get; set; }
+
+        public virtual List<Product> Products { get; set; }
+
+
+    }
+}
