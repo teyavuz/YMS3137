a11d5057b9681980dbc3637b05f14625082136ce
fatih günalp <fatih.gunalp@outlook.com>
2020-03-30 13:31:18 +0300

    .

---
+++ MVC/AspNetMVC/MVCRazorViewNorthwind/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using MVCRazorViewNorthwind.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCRazorViewNorthwind.Controllers
+{
+    public class ProductController : Controller
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        public ActionResult Index()
+        {
+            
+            return View(db.Products.ToList());
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var product = db.Products.FirstOrDefault(x => x.ProductID == id);
+            return View(product);
+        }
+
+    }
+}
