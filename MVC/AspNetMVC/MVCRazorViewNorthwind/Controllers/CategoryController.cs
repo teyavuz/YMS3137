c89b22703051d1ea8d5e7eea9bc0e33a44129ac7
fatih günalp <fatih.gunalp@outlook.com>
2020-03-30 12:23:31 +0300

    created

---
+++ MVC/AspNetMVC/MVCRazorViewNorthwind/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using MVCRazorViewNorthwind.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCRazorViewNorthwind.Controllers
+{
+    public class CategoryController : Controller
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        public ActionResult Index()
+        {
+            List<Category> categories = db.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
