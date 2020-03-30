bba931f59d80e883d506f709b9d6e3ba727edc1a
fatih günalp <fatih.gunalp@outlook.com>
2020-03-30 12:06:21 +0300

    razorView Engine

---
+++ MVC/AspNetMVC/MVCRazorView/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCRazorView.Models;
+
+namespace MVCRazorView.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            List<Category> categories = new List<Category>()
+            {
+                new Category{ID=1,CategoryName="İçecek",Description="İçecekler"},
+                new Category{ID=2,CategoryName="Yiyecek",Description="yiyecekler"},
+                new Category{ID=3,CategoryName="Tatlı",Description="Tatlılar"},
+
+            };
+
+
+            return View(categories);
+        }
+    }
+}
+
---
+++ MVC/AspNetMVC/MVCRazorView/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCRazorView.Controllers
+{
+    public class HomeController : Controller
+    {
+        //View Engine Nedir?
+        /*
+         Razor
+         Web Form
+         Spark
+         Nhalm
+             */
+
+        //Razor View Engine
+
+            /*
+             Asp.Net MVC 3 ile hayatımıza giren Razor View Engine view engine lar arasında en sıklıkla kullanılanıdır. Html ile C# kodlarını iç içe yazmamıza olanaj sağlar.
+             */
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
