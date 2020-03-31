4a457222d444b83fac8b2f0079e425147dde498b
fatih günalp <fatih.gunalp@outlook.com>
2020-03-31 12:56:32 +0300

    partial oluşturuldu.

---
+++ MVC/AspNetMVC/MVCHtmlHelpers/Controllers/ActionController.cs
@@ -0,0 +1,41 @@
+﻿using MVCHtmlHelpers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCHtmlHelpers.Controllers
+{
+    public class ActionController : Controller
+    {
+        // GET: Action
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Action()
+        {
+            return View();
+        }
+        public ActionResult _PartialAction()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult _PartialActionRoute(int id)
+        {
+            List<Sehirler> sehirler = new List<Sehirler>()
+            {
+                new Sehirler{ID=1,SehirAdi="İstanbul"},
+                new Sehirler{ID=2,SehirAdi="Ankara"},
+                new Sehirler{ID=3,SehirAdi="İzmir"}
+
+            };
+            var sehir = sehirler.Where(x => x.ID == id).FirstOrDefault();
+            return PartialView(sehir);
+        }
+
+    }
+}
---
+++ MVC/AspNetMVC/MVCHtmlHelpers/Controllers/HelpController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCHtmlHelpers.Controllers
+{
+    public class HelpController : Controller
+    {
+        // GET: Help
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult Help()
+        {
+            return PartialView();
+        }
+    }
+}
