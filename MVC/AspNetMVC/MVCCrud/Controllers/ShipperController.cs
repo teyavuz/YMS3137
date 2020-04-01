0f7ea94448aa12363a2d5bb92ce3eeb4e39fc0c0
fatih günalp <fatih.gunalp@outlook.com>
2020-04-01 12:27:09 +0300

    Ekleme tasarımı

---
+++ MVC/AspNetMVC/MVCCrud/Controllers/ShipperController.cs
@@ -0,0 +1,32 @@
+﻿using MVCCrud.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCrud.Controllers
+{
+    public class ShipperController : Controller
+    {
+        //Database
+        NorthwindEntities db = new NorthwindEntities();
+
+        // Nakliyecilerin Listelenmesi
+        public ActionResult Index()
+        {
+            return View(db.Shippers.ToList());
+        }
+
+        //Nakliyeci Ekleme
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        //Nakliyeci Güncelleme
+
+        //Nakliyeci Silme
+    }
+}
