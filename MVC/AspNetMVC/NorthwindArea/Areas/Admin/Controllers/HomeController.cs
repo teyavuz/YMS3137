80d22f7884e5b914cdc11a9a64463975030312b0
fatih günalp <fatih.gunalp@outlook.com>
2020-04-07 11:43:47 +0300

    .

---
+++ MVC/AspNetMVC/NorthwindArea/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿
+using NorthwindArea.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NorthwindArea.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        FacebookDBEntities db = new FacebookDBEntities();
+        public ActionResult Index()
+        {
+            ViewBag.Users = db.Users.ToList();
+            ViewBag.Photos = db.Photos.ToList();
+            return View();
+        }
+    }
+}
