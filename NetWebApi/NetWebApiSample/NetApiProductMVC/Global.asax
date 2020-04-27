7ca838861d2365af982ea11655b493e631938f56
fatih günalp <fatih.gunalp@outlook.com>
2020-04-27 12:23:18 +0300

    .

---
+++ NetWebApi/NetWebApiSample/NetApiProductMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NetApiProductMVC.MvcApplication" Language="C#" %>
--- NetWebApi/NetWebApiSample/WebApiProductService/Controllers/ProductController.cs
+++ NetWebApi/NetWebApiSample/WebApiProductService/Controllers/ProductController.cs
@@ -9,6 +9,7 @@ using System.Web.Http;
 
 namespace WebApiProductService.Controllers
 {
+    [Authorize]
     public class ProductController : ApiController
     {
         AppDbContext db = new AppDbContext();
@@ -42,22 +43,67 @@ namespace WebApiProductService.Controllers
 
         }
 
-        public IHttpActionResult ProductOlustur(Product product)
+        //public IHttpActionResult ProductOlustur(Product product)
+        //{
+        //    if (product != null)
+        //    {
+        //        db.Products.Add(product);
+        //        db.SaveChanges();
+        //        return Ok(product);
+        //    }
+        //    else
+        //    {
+        //        return NotFound();
+        //    }
+
+        //}
+        [HttpPost]
+        public HttpResponseMessage CreateProduct(Product product)
         {
             if (product != null)
             {
                 db.Products.Add(product);
                 db.SaveChanges();
-                return Ok(product);
+                return Request.CreateResponse(HttpStatusCode.OK, product);
             }
             else
             {
-                return NotFound();
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound,"ürün boş olamaz");
             }
-
+        }
+        [HttpDelete]
+        public HttpResponseMessage DeleteProduct(int id)
+        {
+            Product product = db.Products.FirstOrDefault(x => x.ID == id);
+            if (product != null)
+            {
+                db.Products.Remove(product);
+                db.SaveChanges();
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, $"{id} numaralı herhangi bir ürün bulunamadı.");
+            }
         }
 
-
+        [HttpPut]
+        public HttpResponseMessage UpdateProduct(int id,Product product)
+        {
+            Product updated = db.Products.FirstOrDefault(x => x.ID == id);
+            if (updated != null)
+            {
+                updated.ProductName = product.ProductName;
+                updated.UnitPrice = product.UnitPrice;
+                updated.UnitsInStock = product.UnitsInStock;
+                db.SaveChanges();
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, $"{id} numaralı herhangi bir ürün bulunamadı.");
+            }
+        }
 
     }
 }
