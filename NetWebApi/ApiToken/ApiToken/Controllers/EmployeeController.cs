0298712a78755f374ded127195445cc7ac52048d
fatih günalp <fatih.gunalp@outlook.com>
2020-04-28 14:11:22 +0300

    .

---
+++ NetWebApi/ApiToken/ApiToken/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using ApiToken.Models;
+using ApiToken.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApiToken.Controllers
+{
+   [Authorize]
+    public class EmployeeController : ApiController
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        
+        public IEnumerable<EmployeeVM> GetEmployees()
+        {
+            return db.Employees.Select(x => new EmployeeVM
+            {
+                FirstName = x.FirstName,
+                LastName = x.LastName
+            }).ToList();
+        }
+    }
+}
