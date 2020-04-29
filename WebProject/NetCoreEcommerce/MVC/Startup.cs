53e8e0c1b34c4ab1406893f26d55116236e71996
fatih günalp <fatih.gunalp@outlook.com>
2020-04-29 12:23:36 +0300

    createdMVC

--- WebProject/NetCoreEcommerce/MVC/Startup.cs
+++ WebProject/NetCoreEcommerce/MVC/Startup.cs
@@ -60,6 +60,10 @@ namespace MVC
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "area",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
