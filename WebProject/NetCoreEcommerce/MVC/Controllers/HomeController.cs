6bd327410f18df1a6d83adf04a32cb80d83440b6
fatih günalp <fatih.gunalp@outlook.com>
2020-04-30 14:03:44 +0300

    created frontend

--- WebProject/NetCoreEcommerce/MVC/Controllers/HomeController.cs
+++ WebProject/NetCoreEcommerce/MVC/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using BLL.Abstract;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MVC.Models;
@@ -12,15 +13,17 @@ namespace MVC.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IProductService productService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IProductService productService)
         {
             _logger = logger;
+            this.productService = productService;
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(productService.GetActive());
         }
 
         public IActionResult Privacy()
